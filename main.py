2cd112da60dd3f2e32a26dcc1c6cd02cad50f3f4
Francisco <matucion1@gmail.com>
2023-03-25 11:58:45 -0300

    Se crearon metodos constructores y clases hijo

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+from car import Car
+from account import Account
+from payment import Payment
+from route import Route
+
 if __name__ =='__main__':
     print("Hello world")
+    '''
+    car = Car()
+    car.license = "AMS123"
+    car.driver = "Andrés Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "JKD423"
+    car2.driver = "Grorge Dam"
+    print(vars(car2))'''
+    car = Car("AMS123", Account("Andrés Herrera", "39447128-4"))
+    print(vars(car))
+    print(vars(car.driver))
